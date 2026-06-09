82e355a29a76e9bb30c6cfcc785a6769d5d52c10
aruizeac <luis.alonso.16@hotmail.com>
2026-06-08 22:59:18 -0600

    add kafka consumer log interceptor

--- streaming/kafkax/interceptor.go
+++ streaming/kafkax/interceptor.go
@@ -27,9 +27,28 @@ func ConsumerInterceptorChain(h Handler, interceptors ...ConsumerInterceptor) Ha
 
 func LogConsumerInterceptor(logger *slog.Logger) ConsumerInterceptor {
 	return func(next Handler) Handler {
-		return func(ctx context.Context, record *kgo.Record) error {
-			logger.InfoContext(ctx, "processing kafka message")
-			return next(ctx, record)
+		return func(ctx context.Context, record *kgo.Record) (err error) {
+			logAttrs := slog.Group("message",
+				slog.String("key", string(record.Key)),
+				slog.Int("val_len", len(record.Value)),
+				slog.Int("total_header_items", len(record.Headers)),
+				slog.Time("timestamp", record.Timestamp),
+				slog.String("topic", record.Topic),
+				slog.Int64("partition", int64(record.Partition)),
+				slog.Int64("producer_id", record.ProducerID),
+				slog.Int64("leader_epoch", int64(record.LeaderEpoch)),
+				slog.Int64("offset", record.Offset),
+			)
+			logger.InfoContext(ctx, "processing kafka record", logAttrs)
+			defer func() {
+				if err != nil {
+					logger.ErrorContext(ctx, "unable to process kafka record", logAttrs)
+					return
+				}
+				logger.InfoContext(ctx, "proccessed kafka record", logAttrs)
+			}()
+			err = next(ctx, record)
+			return
 		}
 	}
 }
